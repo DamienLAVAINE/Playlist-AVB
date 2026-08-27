af95f78c1f90961e694d217c2d1591a5f1d9b71a
DamienLAVAINE <damien.lavaine@live.com>
2026-08-27 13:12:47 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const tracks = [
   { name: "No more/AVB", file: "audio/AVB - No more 13.08.26.mp3", duration: "3:37" },
-  { name: "Electrocardiogramme/AVB", file: "audio/AVB - Electrodardiogramme 13.08-3.mp3", duration: "4:13" },
+  { name: "Electrocardiogramme/AVB", file: "audio/AVB - Electrodardiogramme 27.08.26.mp3", duration: "4:13" },
   { name: "C'est quoi ça/AVB", file: "audio/AVB - C'est quoi ça _ bd 20 (Remix) sans Marion 21.08-2.mp3", duration: "4:19" },
   { name: "Ma Benz/Brigitte", file: "audio/Ma benz 27.08-2.mp3", duration: "2:55" },
   { name: "Pookie/Aya", file: "audio/record pookie 27.08-2.mp3", duration: "2:41" },
